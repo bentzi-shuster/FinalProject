5926b5ae672d03e051b6e9b90270efe5b25d3e7d
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-02-19 14:20:11 -0500

    I did sone js and iframe stuff so i dont need to to repeat the nav bar a lot

---
+++ Scripts/navigation.js
@@ -0,0 +1,15 @@
+const navlinks = document.getElementsByClassName("nav-link");
+for( let i=0; i<navlinks.length;i++){
+    const navlink= navlinks[i];
+    navlink.addEventListener("click",(e)=>{
+        e.preventDefault();
+        if(navlink.classList.contains("active")===true) return;
+        const curPage = document.querySelector(".nav-link.active");
+        curPage.classList.remove("active");
+        curPage.removeAttribute("aria-current");
+        navlink.classList.add("active");
+        navlink.setAttribute("aria-current","page");
+        const frame = document.getElementById("content");
+        frame.src=navlink.href;
+    })
+}
