4e604774b13d912c8ce7be72bc432d70f74bea3b
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-02-21 08:07:05 -0500

    debugging scrollbards

--- Scripts/navigation.js
+++ Scripts/navigation.js
@@ -13,3 +13,15 @@ for( let i=0; i<navlinks.length;i++){
         frame.src=navlink.href;
     })
 }
+const logo=document.getElementsByClassName("logo-link")[0];
+logo.addEventListener("click",(e)=> {
+    e.preventDefault();
+    const home_navlink = document.querySelector('a.nav-link[href="home.html"]');
+    const curPage = document.querySelector(".nav-link.active");
+    curPage.classList.remove("active");
+    curPage.removeAttribute("aria-current");
+    home_navlink.classList.add("active");
+    home_navlink.setAttribute("aria-current", "page");
+    const frame = document.getElementById("content");
+    frame.src = home_navlink.href;
+})
