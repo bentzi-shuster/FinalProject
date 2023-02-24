b361183e7d81501ef002d07f8df277d30fcded57
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-02-24 16:33:16 -0500

    navbar system is revamped to not use iframe, but rather use append child

--- Scripts/navigation.js
+++ Scripts/navigation.js
@@ -1,48 +1,47 @@
-const navlinks = document.getElementsByClassName("nav-link");
-for( let i=0; i<navlinks.length;i++){
-    const navlink= navlinks[i];
-    navlink.addEventListener("pointerdown",(e)=>{
-        e.preventDefault();
-        if(navlink.classList.contains("active")===true) return;
-        const curPage = document.querySelector(".nav-link.active");
-        curPage.classList.remove("active");
-        curPage.removeAttribute("aria-current");
-        navlink.classList.add("active");
-        navlink.setAttribute("aria-current","page");
-        const frame = document.getElementById("content");
-        frame.src=navlink.href;
-        frame.addEventListener("load",()=>{document.title=frame.contentDocument.title})
-    });
-    navlink.addEventListener("pointerup",(e)=>{
-        e.preventDefault();
-    });
-    navlink.addEventListener("click",(e)=>{
-        e.preventDefault();
-    });
-    navlink.addEventListener("auxclick",(e)=>{
-        e.preventDefault();
-    });
-}
-const logo=document.getElementsByClassName("logo-link")[0];
-logo.addEventListener("pointerdown",(e)=> {
-    e.preventDefault();
-    const home_navlink = document.querySelector('a.nav-link[href="home.html"]');
-    const curPage = document.querySelector(".nav-link.active");
-    curPage.classList.remove("active");
-    curPage.removeAttribute("aria-current");
-    home_navlink.classList.add("active");
-    home_navlink.setAttribute("aria-current", "page");
-    const frame = document.getElementById("content");
-    frame.src = home_navlink.href;
-    frame.addEventListener("load",()=>{document.title=frame.contentDocument.title});
+let appendElm = document.createElement("div")
+appendElm.innerHTML=`
+<header id="header-headerElm">
+    <div class="d-flex flex-row ">
+        <a class="my-auto mx-4 logo-link"  href="home.html">
+            <img src="Images/logo.png"  width="50" height="50" alt="logo of alcohol bottle and wine glass">
+        </a>
+        <span class="h4 fw-bolder p-3 d-block"> Tipsy Testing </span>
+    </div>
+<!--https://getbootstrap.com/docs/5.3/components/navbar/-->
+<nav id="navbar-navElm" class="navbar navbar-expand-lg bg-body-tertiary ">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav w-100 d-flex justify-content-evenly">
 
-});
-logo.addEventListener("pointerup",(e)=>{
-    e.preventDefault();
-});
-logo.addEventListener("click",(e)=>{
-    e.preventDefault();
-});
-logo.addEventListener("auxclick",(e)=>{
-    e.preventDefault();
-});
+                <li class="fw-bold nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+                </li>
+
+
+                <li class="fw-bold nav-item">
+                    <a class="nav-link" href="contact.html">Contact Us</a>
+                </li>
+                <li class="fw-bold nav-item">
+                    <a class="nav-link" href="tips.html">Study Tips</a>
+                </li>
+                <li class="fw-bold nav-item">
+                    <a class="nav-link" href="smuggling.html">Smuggling 101</a>
+                </li>
+                <li class="fw-bold nav-item">
+                    <a class="nav-link" href="alternatives.html">Alternatives for under 21</a>
+                </li>
+                <li class="fw-bold nav-item">
+                    <a class="nav-link" href="testimonies.html">Testimonies</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</nav>
+
+</header>
+`
+document.body.appendChild(appendElm);
